91aee487a1183912019716b27d48d629a7ce23dd
‍김용빈 <yongbin97@yonsei.ac.kr>
2023-02-09 00:12:57 +0900

    [FIX]: diary edit 수정

--- src/test/java/kr/co/aeye/apiserver/ApiServerApplicationTests.java
+++ src/test/java/kr/co/aeye/apiserver/ApiServerApplicationTests.java
@@ -1,7 +1,7 @@
 package kr.co.aeye.apiserver;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest;    
 
 @SpringBootTest
 class ApiServerApplicationTests {
