90d6d41b9c321d7aba3fc042d29bc252b53d3833
‍김용빈 <yongbin97@yonsei.ac.kr>
2023-03-24 09:37:05 +0900

    [CHORE] : health check

--- src/main/java/kr/co/aeye/apiserver/api/main/MainController.java
+++ src/main/java/kr/co/aeye/apiserver/api/main/MainController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping
+@RequestMapping("/")
 public class MainController {
     private final MainService mainService;
     @GetMapping
