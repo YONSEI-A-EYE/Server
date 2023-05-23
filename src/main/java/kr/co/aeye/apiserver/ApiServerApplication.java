e610adc6f98177f5d1397f7758ee4bf2fb865dc4
‍김용빈 <yongbin97@yonsei.ac.kr>
2023-05-23 23:00:48 +0900

    [CHORE] update

--- src/main/java/kr/co/aeye/apiserver/ApiServerApplication.java
+++ src/main/java/kr/co/aeye/apiserver/ApiServerApplication.java
@@ -2,11 +2,8 @@ package kr.co.aeye.apiserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class ApiServerApplication {
 
     public static void main(String[] args) {
